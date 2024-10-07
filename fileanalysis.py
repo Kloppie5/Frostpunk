52d2bb490ea49b1c619ff9b7e4405ef3d20724f5
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2024-10-07 20:41:49 +0200

    Finding structure

--- fileanalysis.py
+++ fileanalysis.py
@@ -1,20 +1,36 @@
+import struct
 
 def main():
-    hexdump_file("code/common.dat")
-    hexdump_file("code/common.idx")
-    hexdump_file("saves/NEW HOPE.save")
+    hexdump_file("code/common.dat", end = 0x300)
+    extract_data_from_idx("code/common.idx")
+    hexdump_file("saves/NEW HOPE.save", end = 0x300)
 
-def hexdump_file(filepath):
+def hexdump_file(filepath, start = 0, end = 0, step = 48):
     with open(filepath, 'rb') as f:
         data = f.read()
-    offset = 0
+    offset = start
+    if end == 0 :
+        end = len(data)
     print(f"==================== {filepath} ====================")
-    while offset < 1000:#len(data):
-        chunk = data[offset : offset + 48]
+    while offset < end:
+        chunk = data[offset : offset + step]
         hex_values = " ".join(f"{byte:02x}".replace("0", "-") for byte in chunk)
         ascii_values = "".join(chr(byte) if 32 <= byte <= 126 else " " for byte in chunk)
         print(f"{offset:08x}  {hex_values:<96} | {ascii_values} |")
-        offset += 48
+        offset += step
+
+def extract_data_from_idx(filepath):
+    with open(filepath, 'rb') as f:
+        data = f.read()
+    offset = 0
+    
+    offset += 3 # 00 02 01 "Signature"
+    filecount, = struct.unpack('<Q', data[offset:offset+8])
+    offset += 8
+    for i in range(filecount) :
+        s1, s2, s3, s4, s5 = struct.unpack('<IQQQ?', data[offset:offset+29])
+        print(f"{i}: {s1} {s2} {s3} {s4} {s5}")
+        offset += 29
 
 if __name__ == "__main__" :
     main()
